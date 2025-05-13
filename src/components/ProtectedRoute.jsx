1bb5c6763a836b254f4986f0072d322c9748ced6
beingayandey <ayandey166@gmail.com>
2025-05-13 20:51:33 +0530

    protected route chnaged

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,11 +1,12 @@
-import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const ProtectedRoute = ({ children }) => {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
 
   if (!isAuthenticated) {
-    return <Navigate to="/" replace />;
+    // Redirect to /login if not authenticated
+    return <Navigate to="/login" replace />;
   }
 
   return children;
--- src/layouts/MainLayouts.jsx
+++ src/layouts/MainLayouts.jsx
@@ -1,11 +1,10 @@
 import { Route, Routes, useLocation } from "react-router-dom";
-
 import Login from "../pages/Login";
-
 import ChatPage from "../pages/ChatPage";
 import Header from "../components/Header";
 import { ToastProvider } from "../components/contexts/ToastNotification";
 import Dashboard from "../pages/Dashboard";
+import ProtectedRoute from "../components/ProtectedRoute"; // Import the new component
 
 const MainLayouts = () => {
   const location = useLocation();
@@ -18,16 +17,25 @@ const MainLayouts = () => {
       <div className="main-body-outer">
         <Routes>
           <Route path="/" element={<Login />} />
-          <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
           <Route
             path="/chat/:userId"
             element={
-              <div className="app-layout">
-                <div className="main-content">
-                  <ChatPage />
+              <ProtectedRoute>
+                <div className="app-layout">
+                  <div className="main-content">
+                    <ChatPage />
+                  </div>
                 </div>
-              </div>
+              </ProtectedRoute>
             }
           />
         </Routes>
